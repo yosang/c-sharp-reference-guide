0ca35989ac6028b1f2b73f99a4f6c5330725a0a7
Yosmel Chiang <yosmelchiang@gmail.com>
2026-02-11 07:20:57 +0100

    initial commit

---
+++ array-fun/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+int[] myArr = [1, 2, 4, 5];
+
+// int[] sliced = myArr[2..]; // Slices the array from index 2 and to the end (exclusive)
+// int[] sliced = myArr[1..2]; // Gets 2
+// int[] sliced = myArr[..2]; // Gets 1 and 2
+// int[] sliced = myArr[2..]; // Gets 4 and 5
+
+// foreach (int i in sliced)
+// {
+//     Console.WriteLine(i);
+// }
+
+int last = myArr[^1]; // Gets the last number
+// int last = myArr[^2]; // Gets the seconbd last number
+
+Console.WriteLine(last);
